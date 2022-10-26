dbef4454f3caf3e2b090ab69b43476c7074e4810
TeoEstudio <dei.guiribalde@gmail.com>
2022-10-25 22:52:57 -0500

    Vista Mis documentos
    Maquetación de vista "Mis documentos"

--- resources/views/documentos/docs.blade.php
+++ resources/views/documentos/docs.blade.php
@@ -4,48 +4,79 @@
 
  @section('content')
 
+ <div class="card br-16">
+    <div class="meetup-img-wrapper rounded-top text-center">
+        <img class="card-img-top br-16" src="{{asset('app-assets/images/portada2.jpg')}}" alt="Card image cap" style="object-fit:cover;" height="270">
+    </div>
+</div>
 
-                <div class="flex items-center mt-8">
-                    <h2 class="intro-y text-lg font-medium mr-auto">
-                        MIS DOCUMENTOS
-                    </h2>
-                </div>
-                <div class="border border-slate-200/60 dark:border-darkmode-400 rounded-md p-5  box ">
-                   
-                            <div class="intro-y flex flex-col-reverse sm:flex-row items-center">
-                                <h2 class="intro-y text-lg font-medium mr-auto mt-2">
-                                    File Manager
-                                </h2>
-                                <div class="w-full sm:w-auto flex items-right ">
-                                  <input type="file" class="top-0 left-0 absolute button">
-                                    <button class="btn btn-primary shadow-md mr-2">Subir documentos</button>
-                                </div>
-                            </div>
-                            <div class="border-2 border-dashed dark:border-darkmode-400 rounded-md pt-4 mt-3">
-                                <div class="flex flex-wrap px-4">
-                                    <div class="w-24 h-24 relative image-fit mb-5 mr-5 cursor-pointer zoom-in">
-                                        <img class="rounded-md" alt="Midone - HTML Admin Template" src="{{asset('images/preview-5.jpg')}}">
-                                        <div title="Remove this image?" class="tooltip w-5 h-5 flex items-center justify-center absolute rounded-full text-white bg-danger right-0 top-0 -mr-2 -mt-2"> <i data-lucide="x" class="w-4 h-4"></i> </div>
-                                    </div>
-                                    <div class="w-24 h-24 relative image-fit mb-5 mr-5 cursor-pointer zoom-in">
-                                        <img class="rounded-md" alt="Midone - HTML Admin Template" src="{{asset('images/preview-5.jpg')}}">
-                                        <div title="Remove this image?" class="tooltip w-5 h-5 flex items-center justify-center absolute rounded-full text-white bg-danger right-0 top-0 -mr-2 -mt-2"> <i data-lucide="x" class="w-4 h-4"></i> </div>
-                                    </div>
-                                    <div class="w-24 h-24 relative image-fit mb-5 mr-5 cursor-pointer zoom-in">
-                                        <img class="rounded-md" alt="Midone - HTML Admin Template" src="{{asset('images/preview-5.jpg')}}">
-                                        <div title="Remove this image?" class="tooltip w-5 h-5 flex items-center justify-center absolute rounded-full text-white bg-danger right-0 top-0 -mr-2 -mt-2"> <i data-lucide="x" class="w-4 h-4"></i> </div>
-                                    </div>
-                                    <div class="w-24 h-24 relative image-fit mb-5 mr-5 cursor-pointer zoom-in">
-                                        <img class="rounded-md" alt="Midone - HTML Admin Template" src="{{asset('images/preview-5.jpg')}}">
-                                        <div title="Remove this image?" class="tooltip w-5 h-5 flex items-center justify-center absolute rounded-full text-white bg-danger right-0 top-0 -mr-2 -mt-2"> <i data-lucide="x" class="w-4 h-4"></i> </div>
-                                    </div>
-                                </div>
-                                <div class="px-4 pb-4 flex items-center cursor-pointer relative">
-                                    <i data-lucide="image" class="w-4 h-4 mr-2"></i> <span class="text-primary mr-1">Upload a file</span> or drag and drop 
-                                    <input type="file" class="w-full h-full top-0 left-0 absolute opacity-0">
-                                </div>
-                            </div>
-                </div>
+<div class="card br-16 p-36">
+    <div class="card-body">
+        <h2 class="intro-y text-lg font-medium mr-auto font-weight-bold">
+            MIS DOCUMENTOS
+        </h2>
+        <h5 class="intro-y mr-auto">
+            Por favor, llene todos los documentos a continuación, luego darle click a enviar para proceder con la postulación:
+        </h5>
+
+        <table class="table  table-bordered mt-24">
+            <thead>
+                <tr>
+                    <th>DOCUMENTO</th>
+                    <th>SUBIR ARCHIVO</th>
+                    <th>ARCHIVOS SUBIDOS</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="text-pri font-weight-bold">CERTIFICADO DE ESTUDIOS</td>
+                    <td>
+                        <button id="select-files" class="btn btn-outline-primary mb-1 waves-effect dz-clickable">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg> Subir documento
+                </button>
+            </td>
+                    <td>certificado.jpg</td>
+                </tr>
+                <tr>
+                    <td class="text-pri font-weight-bold">CONSTANCIAS DE TRABAJOS</td>
+                    <td>
+                        <button id="select-files" class="btn btn-outline-primary mb-1 waves-effect dz-clickable">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg> Subir documento
+                </button>
+            </td>
+                    <td>certificado.jpg</td>
+                </tr>
+                <tr>
+                    <td class="text-pri font-weight-bold">ANTECEDENTES</td>
+                    <td>
+                        <button id="select-files" class="btn btn-outline-primary mb-1 waves-effect dz-clickable">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg> Subir documento
+                </button>
+            </td>
+                    <td>certificado.jpg</td>
+                </tr>
+                <tr>
+                    <td class="text-pri font-weight-bold">DNI</td>
+                    <td>
+                        <button id="select-files" class="btn btn-outline-primary mb-1 waves-effect dz-clickable">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg> Subir documento
+                </button>
+            </td>
+                    <td>certificado.jpg</td>
+                </tr>
+                <tr>
+                    <td class="text-pri font-weight-bold">CERTIFICACIÓN DE RENTA 5TA CATEGORÍA</td>
+                    <td>
+                        <button id="select-files" class="btn btn-outline-primary mb-1 waves-effect dz-clickable">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg> Subir documento
+                </button>
+            </td>
+                    <td>certificado.jpg</td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
                 </div>
 
  @endsection
